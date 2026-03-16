155adaaaf7015ddea8cfa8eb8d9bcf67f2667a54
Somesh Nukala <somesh.nukala@contractor.optavia.com>
2026-03-16 23:11:46 +0530

    import issue fix in GIV.tsx

--- src/pages/GIV/GIV.tsx
+++ src/pages/GIV/GIV.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import { metricsData } from './chartData'; 
 import styles from './GIV.module.scss';
