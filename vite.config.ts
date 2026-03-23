c80d810ca9826a2fbdd2af39a98bb2d40d5bd07f
Somesh Nukala <somesh.nukala@contractor.optavia.com>
2026-03-23 15:04:04 +0530

    refactor: centralize scss variables and resolve module dependency errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // We use path.resolve to find the exact file on your Mac
+        additionalData: `@use "${path.resolve(__dirname, "src/styles/_variables.scss")}" as *;`,
+        api: 'modern-compiler'
+      },
+    },
+  },
+});
